2b5b38c1e15373bf3456c7a684364287f7614a8e
Desmond Odame <57676320+yewest@users.noreply.github.com>
2023-06-26 17:26:56 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -2,14 +2,22 @@
 let jobData = [];
 
 // Function to fetch data from JSON file
-function fetchJobData() {
-    fetch('data.json')
-        .then(response => response.json())
-        .then(data => {
-            jobData = data.jobs;
-            displayJobs(jobData);
-        })
-        .catch(error => console.log('Error fetching job data:', error));
+async function fetchJobData() {
+    try {
+        const response = await fetch('data.json');
+        const data = await response.json();
+        jobData = data.jobs;
+        displayJobs(jobData);
+    } catch (error) {
+        console.log('Error fetching job data:', error);
+    }
+}
+
+// Function to create an element with given tag name and text content
+function createElement(tagName, textContent) {
+    const element = document.createElement(tagName);
+    element.textContent = textContent;
+    return element;
 }
 
 // Function to display job listings
@@ -21,17 +29,9 @@ function displayJobs(jobs) {
         const jobElement = document.createElement('div');
         jobElement.classList.add('job');
 
-        const titleElement = document.createElement('h3');
-        titleElement.textContent = job.title;
-        jobElement.appendChild(titleElement);
-
-        const locationElement = document.createElement('p');
-        locationElement.textContent = 'Location: ' + job.location;
-        jobElement.appendChild(locationElement);
-
-        const typeElement = document.createElement('p');
-        typeElement.textContent = 'Employment Type: ' + job.employmentType;
-        jobElement.appendChild(typeElement);
+        jobElement.appendChild(createElement('h3', job.title));
+        jobElement.appendChild(createElement('p', 'Location: ' + job.location));
+        jobElement.appendChild(createElement('p', 'Employment Type: ' + job.employmentType));
 
         // View Details button
         const viewButton = document.createElement('button');
@@ -45,12 +45,12 @@ function displayJobs(jobs) {
 
 // Function to filter jobs based on user selections
 function filterJobs() {
-    const titleFilter = document.getElementById('title-filter').value;
+    const titleFilter = document.getElementById('title-filter').value.toLowerCase();
     const locationFilter = document.getElementById('location-filter').value;
     const typeFilter = document.getElementById('type-filter').value;
 
     const filteredJobs = jobData.filter(job => {
-        const titleMatch = job.title.toLowerCase().includes(titleFilter.toLowerCase());
+        const titleMatch = job.title.toLowerCase().includes(titleFilter);
         const locationMatch = locationFilter === '' || job.location === locationFilter;
         const typeMatch = typeFilter === '' || job.employmentType === typeFilter;
 
@@ -60,66 +60,60 @@ function filterJobs() {
     displayJobs(filteredJobs);
 }
 
-// Function to open a new window with job details
+// Function to open a new tab with job details
 function openJobDetailsWindow(index) {
     const job = jobData[index];
 
-    const windowFeatures = 'width=600,height=400,resizable=yes';
-    const jobDetailsWindow = window.open('', '_blank', windowFeatures);
     const jobDetailsHTML = `
         <html>
         <head>
             <title>Job Details</title>  
-            <style>
-                /* CSS styles for the new window */
-                body {
-                    font-family: Arial, sans-serif;
-                    background-color: #f5f5f5;
-                    padding: 20px;
-                }
-
-                h3 {
-                    color: #333;
-                }
-
-                p {
-                    color: #666;
-                }
-
-                button {
-                    background-color: #007bff;
-                    color: #fff;
-                    padding: 10px 20px;
-                    border: none;
-                    cursor: pointer;
-                }
-
-                button:hover {
-                    background-color: #0056b3;
-                }
-            </style
-            
+            <link rel="stylesheet" type="text/css" href="style.css">
         </head>
         <body>
-            <h3>${job.title}</h3>
-            <p>Location: ${job.location}</p>
-            <p>Employment Type: ${job.employmentType}</p>
-            <p>Description: ${job.description}</p>
-            <button onclick="applyForJob(${index})">Apply</button>
+            <div class="job-details-body">
+    <h3>${job.title}</h3>
+    <div class="job-info">
+        <p>Location: ${job.location}</p>
+        <p>Employment Type: ${job.employmentType}</p>
+    </div>
+    <div class="description-container">
+        <p>Description: ${job.description}</p>
+    </div>
+    <div class="application-container">
+        <h4>Application Form</h4>
+        <label for="name">Name:</label>
+        <input type="text" id="name">
+        <label for="email">Email:</label>
+        <input type="email" id="email">
+        <label for="files">Attach Files:</label>
+        <input type="file" id="files" multiple>
+        <button onclick="applyForJob(${index})">Apply</button>
+    </div>
+            <script src="script.js"></script>
         </body>
         </html>
     `;
 
+    const jobDetailsWindow = window.open('', '_blank');
     jobDetailsWindow.document.open();
     jobDetailsWindow.document.write(jobDetailsHTML);
     jobDetailsWindow.document.close();
 }
 
-// Function to handle applying for a job
-function applyForJob(index) {
-    const job = jobData[index];
-    // Implement your logic to handle the job application, e.g., opening a new page or form
-    console.log('Applying for Job:', job);
+// Function to submit job application
+function submitApplication(event, index) {
+    event.preventDefault();
+
+    const form = document.getElementById('application-form');
+    const name = form.name.value;
+    const email = form.email.value;
+    const resume = form.resume.files[0];
+
+    console.log('Application submitted for job index', index);
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Resume:', resume);
 }
 
 // Read data when the page loads
